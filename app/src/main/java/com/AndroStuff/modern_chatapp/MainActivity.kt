5ed07f5fcfa8a0f1abb4adba60f8d8552fd94480
AndroStuff <androstuff007@gmail.com>
2021-11-26 04:24:18 -0800

    image picker

--- app/src/main/java/com/AndroStuff/modern_chatapp/MainActivity.kt
+++ app/src/main/java/com/AndroStuff/modern_chatapp/MainActivity.kt
@@ -1,22 +1,31 @@
 package com.AndroStuff.modern_chatapp
 
+import android.app.Activity
 import android.content.ContentValues.TAG
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.*
 import com.google.firebase.ktx.Firebase
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 import java.util.*
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
+    //Declaration
     private lateinit var mobilenumber : EditText
     private lateinit var entereedOtp : EditText
     private lateinit var sendOtpButton : Button
@@ -28,12 +37,26 @@ class MainActivity : AppCompatActivity() {
     private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
     var storedVerificationId:String = ""
     lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+    private val PICK_CODE = 1000;
+    var permissionGranted : Boolean = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this,android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        {
+
+            ActivityCompat.requestPermissions(this, Array(2){android.Manifest.permission.READ_EXTERNAL_STORAGE
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE},111)
+        }else{
+            permissionGranted = true
+        }
+
+
+        //Initialization
         auth = FirebaseAuth.getInstance()
 
         mobilenumber = findViewById(R.id.user_mobile_number)
@@ -44,6 +67,9 @@ class MainActivity : AppCompatActivity() {
         Profile = findViewById(R.id.profile)
         uname = findViewById(R.id.user_name)
         setup = findViewById(R.id.setup)
+
+
+        //Click Listeners
         sendOtpButton.setOnClickListener {
 
             if(mobilenumber.text.isNotEmpty()){
@@ -73,6 +99,10 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        Profile.setOnClickListener {
+            fetchImage()
+        }
+
         callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
             override fun onCodeSent(p0: String, p1: PhoneAuthProvider.ForceResendingToken) {
 
@@ -94,6 +124,19 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+
+
+    }
+
+
+    //Methods
+    private fun fetchImage() {
+        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent,PICK_CODE)
+
+
     }
 
     private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
@@ -140,5 +183,41 @@ class MainActivity : AppCompatActivity() {
             setup.visibility = View.VISIBLE
         }
 
+    }
+
+
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            permissionGranted = true
+        }else{
+            Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            permissionGranted = false
+
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == PICK_CODE){
+            Profile.setImageURI(data?.data)
+            val external = Environment.getExternalStorageState()
+         /*   if(external.equals(Environment.MEDIA_MOUNTED)){
+
+                val sd = Environment.getExternalStorageDirectory().toString()
+                val file = File(sd,"image")
+                try {
+                    val ssteam:OutputStream = FileOutputStream(file)
+                    va
+                }catch ()
+
+            }else{
+                Toast.makeText(this, "Cannot access internal storage", Toast.LENGTH_SHORT).show()
+
+            }*/
+        }
     }
 }
